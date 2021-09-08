1dd58afaec2d32094f18c8143a686f525379718f
davips <dpsabc@gmail.com>
2021-09-08 08:17:16 -0300

    Replace f by fun
    Reason for autoreadme broken is unknown

--- examples/compositionofsets.py
+++ examples/compositionofsets.py
@@ -19,40 +19,40 @@ def h(z, c=[1, 2, 3]):
 # In the ldict framework 'data is function',
 # so the alias ø represents the 'empty data object' and the 'reflexive function' at the same time.
 # In other words: 'inserting nothing' has the same effect as 'doing nothing'.
-f = ø * g * h  # ø enable the cartesian product of the subsequent sets of functions within the expression.
-print(f)
+fun = ø * g * h  # ø enable the cartesian product of the subsequent sets of functions within the expression.
+print(fun)
 # ...
 
 # The difference between 'ø * g * h' and 'ldict(x=3) >> g >> h' is that the functions in the latter are already applied
 # (resulting in an ldict object). The former still has its free parameters unsampled,
 # and results in an ordered set of composite functions.
 # It is a set because the parameter values of the functions are still undefined.
-d = {"x": 5, "y": 7} >> f
+d = {"x": 5, "y": 7} >> fun
 print(d)
 # ...
 
 print(d.z)
 # ...
 
-d = {"x": 5, "y": 7} >> f
+d = {"x": 5, "y": 7} >> fun
 print(d.z)
 # ...
 
 # Reproducible different runs by passing a stateful random number generator.
 rnd = Random(0)
-e = d >> cfg()(rnd=rnd) >> f
+e = d >> cfg()(rnd=rnd) >> fun
 print(e.z)
 # ...
 
-e = d >> cfg()(rnd=rnd) >> f
+e = d >> cfg()(rnd=rnd) >> fun
 print(e.z)
 # ...
 
 rnd = Random(0)
-e = d >> cfg()(rnd=rnd) >> f
+e = d >> cfg()(rnd=rnd) >> fun
 print(e.z)
 # ...
 
-e = d >> cfg()(rnd=rnd) >> f
+e = d >> cfg()(rnd=rnd) >> fun
 print(e.z)
 # ...
--- examples/sampling.py
+++ examples/sampling.py
@@ -9,7 +9,7 @@ from ldict.cfg import cfg
 # 'a' is sampled from an arithmetic progression
 # 'b' is sampled from a geometric progression
 # Here, the syntax for default parameter values is borrowed with a new meaning.
-def f(x, y, a=[-100, -99, -98, ..., 100], b=[0.0001, 0.001, 0.01, ..., 100000000]):
+def fun(x, y, a=[-100, -99, -98, ..., 100], b=[0.0001, 0.001, 0.01, ..., 100000000]):
     return {"z": a * x + b * y}
 
 
@@ -25,51 +25,51 @@ d.show(colored=False)
 # ...
 
 # Parameter values are uniformly sampled.
-# d1 = d >> f
-# d.show(colored=False)
+d1 = d >> fun
+d.show(colored=False)
 # ...
-#
-# print(d1.z)
-# # ...
-#
-# d2 = d >> f
-# d.show(colored=False)
-# # ...
-#
-# print(d2.z)
-# # ...
-#
-# # Parameter values can also be manually set.
-# e = d >> cfg(a=5, b=10) >> f
-# print(e.z)
-# # ...
-#
-# # Not all parameters need to be set.
-# e = d >> cfg(a=5) >> f
-# print(e.z)
-# # ...
-#
-# # Each run will be a different sample for the missing parameters.
-# e = e >> cfg(a=5) >> f
-# print(e.z)
-# # ...
-#
-# # The metaparameter 'rnd' defines the initial state of the random sampler for this point onwards processing the ldict.
-# e = d >> cfg(a=5)(rnd=0) >> f
-# print(e.z)
-# # ...
-#
-# # All runs will yield the same result, if starting from the same random number generator seed.
-# e = e >> cfg(a=5)(rnd=0) >> f
-# print(e.z)
-# # ...
-#
-# # Reproducible different runs are achievable by passing a stateful random number generator, instead of a seed.
-# rnd = Random(0)
-# e = d >> cfg(a=5)(rnd=rnd) >> f
-# print(e.z)
-# # ...
-#
-# e = d >> cfg(a=5)(rnd=rnd) >> f
-# print(e.z)
-# # ...
+
+print(d1.z)
+# ...
+
+d2 = d >> fun
+d.show(colored=False)
+# ...
+
+print(d2.z)
+# ...
+
+# Parameter values can also be manually set.
+e = d >> cfg(a=5, b=10) >> fun
+print(e.z)
+# ...
+
+# Not all parameters need to be set.
+e = d >> cfg(a=5) >> fun
+print(e.z)
+# ...
+
+# Each run will be a different sample for the missing parameters.
+e = e >> cfg(a=5) >> fun
+print(e.z)
+# ...
+
+# The metaparameter 'rnd' defines the initial state of the random sampler for this point onwards processing the ldict.
+e = d >> cfg(a=5)(rnd=0) >> fun
+print(e.z)
+# ...
+
+# All runs will yield the same result, if starting from the same random number generator seed.
+e = e >> cfg(a=5)(rnd=0) >> fun
+print(e.z)
+# ...
+
+# Reproducible different runs are achievable by passing a stateful random number generator, instead of a seed.
+rnd = Random(0)
+e = d >> cfg(a=5)(rnd=rnd) >> fun
+print(e.z)
+# ...
+
+e = d >> cfg(a=5)(rnd=rnd) >> fun
+print(e.z)
+# ...
