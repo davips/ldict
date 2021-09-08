2c29c98c659a0a082c3d464deb9388d28111552e
davips <dpsabc@gmail.com>
2021-09-07 23:13:55 -0300

    Fix examples

--- examples/apply.py
+++ examples/apply.py
@@ -5,9 +5,12 @@ a = ldict(x=3)
 print(a)
 # ...
 
-b = ldict(y=5)
-print(b)
+a = a >> ldict(y=5) >> {"z": 7} >> (lambda x, y, z: {"r": x ** y // z})
+print(a)
 # ...
 
-print(a >> b)
+print(a.r)
+# ...
+
+print(a)
 # ...
--- examples/merging.py
+++ examples/merging.py
@@ -4,3 +4,10 @@ from ldict import ldict
 a = ldict(x=3)
 print(a)
 # ...
+
+b = ldict(y=5)
+print(b)
+# ...
+
+print(a >> b)
+# ...
