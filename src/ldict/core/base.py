e507f1076ac2700a758cf25700e44fd1e45aec2b
davips <dpsabc@gmail.com>
2021-11-18 17:49:31 -0300

    Add attr alias for metafields for AbstractMutableLazyDict also
    Signed-off-by: davips <dpsabc@gmail.com>

--- src/ldict/core/base.py
+++ src/ldict/core/base.py
@@ -82,6 +82,8 @@ class AbstractMutableLazyDict(AbstractLazyDict, ABC):
         self.frozen >>= {key: value}
 
     def __getattr__(self, item):
+        if (metafield := "_" + item) in self.frozen:
+            return self.frozen[metafield]
         return self.frozen[item]
 
     def __repr__(self):
